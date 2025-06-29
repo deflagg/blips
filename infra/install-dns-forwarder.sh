ab604cfa17f0379f370f5ddaf54553cb331d140e
Dennis Flagg <deflagg@hotmail.com>
2025-06-29 14:39:39 -0400

    feat: Enhance dnsmasq installation script with clearer echo statements and improved conflict handling

--- infra/install-dns-forwarder.sh
+++ infra/install-dns-forwarder.sh
@@ -9,18 +9,23 @@ sudo apt update -y
 echo "Installing dnsmasq..."
 sudo apt install -y dnsmasq
 
-echo "Purging resolvconf..."
+echo "Purging resolvconf so it doesn't conflict with dnsmasq..."
 sudo apt purge -y resolvconf
 
-echo "Configuring dnsmasq..."
-sudo tee /etc/dnsmasq.conf >/dev/null <<'CONF'
+echo "Purging systemd-resolved so it doesn't conflict with dnsmasq..."
+sudo apt purge -y systemd-resolved
+
+echo "Configuring dnsmasq to forward DNS requests..."
+sudo tee -a /etc/dnsmasq.conf >/dev/null <<'CONF'
 no-resolv
 server=168.63.129.16
 interface=eth0
 CONF
 
-echo "Updating resolv.conf..."
+echo "Updating resolv.conf to use dnsmasq..."
 echo "nameserver 127.0.0.1" | sudo tee /etc/resolv.conf
+
+echo "Updating /etc/hosts to include dnsforwarder..."
 sudo sed -i 's/127\.0\.0\.1 localhost/127.0.0.1 localhost dnsforwarder/' /etc/hosts
 
 echo "Enabling & starting dnsmasq..."

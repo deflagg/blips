6107c14dcf4c3eef20ee183f3f2c519e0f732017
Dennis Flagg <deflagg@hotmail.com>
2025-06-29 00:17:47 -0400

    feat: Enhance DNS forwarder installation script with additional output formatting for better visibility

--- infra/deploy.ps1
+++ infra/deploy.ps1
@@ -24,7 +24,7 @@ az stack group create `
 #    --parameters     @$ParametersFile
 
 
-Write-Host "`n➤ Installing DNS forwarder on new VM ..."
+Write-Host "➤➤➤➤➤➤ Installing DNS forwarder on new VM ..."
 $DNS_FORWARDER_VM_PRIVATE_KEY = $env:DNS_FORWARDER_VM_PRIVATE_KEY
 # call dns forwarder script with the private key
 write-host "writing private key to file"
@@ -32,20 +32,26 @@ Set-Content -Path '.\dnsforwarederprivatekey.pem' -Value $DNS_FORWARDER_VM_PRIVA
 ssh-keygen -R 10.1.0.36    
 ssh -i '.\dnsforwarederprivatekey.pem' azureuser@10.1.0.36
 
-echo "Updating apt ..."
+echo "➤➤➤➤➤➤ Updating apt ..."
 sudo apt update
-echo "Installing dnsmasq ..."
+echo "➤➤➤➤➤➤ Installing dnsmasq ..."
 sudo apt install -y dnsmasq
-echo "Purging resolvconf ..."
+echo "➤➤➤➤➤➤ Purging resolvconf ..."
 sudo apt purge resolvconf
 sudo -i
+
+echo "➤➤➤➤➤➤ Configuring dnsmasq ..."
 echo "no-resolv" | sudo tee -a /etc/dnsmasq.conf
 echo "server=168.63.129.16" | sudo tee -a /etc/dnsmasq.conf
 echo "interface=eth0" | sudo tee -a /etc/dnsmasq.conf
 
+echo "➤➤➤➤➤➤ Updating resolv.conf ..."
 sudo rm /etc/resolv.conf
 echo "nameserver 127.0.0.1" | sudo tee -a /etc/resolv.conf
 sudo sed -i 's/127.0.0.1 localhost/127.0.0.1 localhost dnsforwarder/' /etc/hosts
 
+echo "➤➤➤➤➤➤ Restarting dnsmasq service ..."
 sudo systemctl start dnsmasq
+
+echo "➤➤➤➤➤➤ Enabling dnsmasq service to start on boot ..."
 sudo systemctl enable dnsmasq

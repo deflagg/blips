0fdf179b056079f0196a637a23c4fd88e44fedf1
Dennis Flagg <deflagg@hotmail.com>
2025-07-18 13:58:37 -0400

    fix: Update import and usage of NewsFeed component to BlipFeed in App.jsx

--- ui/src/App.jsx
+++ ui/src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import NewsFeed from './NewsFeed';  // Import the new component
+import BlipFeed from './components/blipfeed';  // Import the new component
 
 function App() {
   const [count, setCount] = useState(0)
@@ -29,7 +29,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
-      <NewsFeed />  // Add the NewsFeed component here
+      <BlipFeed />  // Add the NewsFeed component here
     </>
   )
 }
